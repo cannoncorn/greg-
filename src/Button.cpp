e987631861d295e99304be7addfca8f098e6f532
tobychadderton1 <hapaxemail@mail.com>
2019-11-06 21:31:39 +0000

    code cleanup

--- src/Button.cpp
+++ src/Button.cpp
@@ -7,7 +7,9 @@ bool Button::isClicked(sf::Window &win)
     sf::Vector2i mousePos = sf::Mouse::getPosition(win);
 
     // if clicked and delay happened
-    if (getGlobalBounds().contains(mousePos.x, mousePos.y) & sf::Mouse::isButtonPressed(sf::Mouse::Left) & clickClock.getElapsedTime().asSeconds() > 0.2)
+    if (getGlobalBounds().contains(mousePos.x, mousePos.y)
+    & sf::Mouse::isButtonPressed(sf::Mouse::Left)
+    & clickClock.getElapsedTime().asSeconds() > 0.2)
     {
         clickClock.restart(); // restart click clock
 
